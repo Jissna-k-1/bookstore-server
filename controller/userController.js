540fda0d9103176f957010c4993cddc821a66cb0
Jissna-k-1 <jissnamathai@gmail.com>
2026-06-21 00:17:07 +0530

    next commit

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,7 @@
 const users = require('../models/userModel')
+// jsonwebtoken
+const jwt = require('jsonwebtoken')
+
 
 // register api request
 exports.registerController =async (req,res)=>{
@@ -18,6 +21,35 @@ exports.registerController =async (req,res)=>{
          res.status(200).json(newUser)
       }
    } catch (error) {
+      console.log(error);
+      res.status(500).json(error)
+      
+   }
+
+   // res.status(200).json("Request recieved")
+   
+}
+
+// login api
+exports.loginController =async (req,res)=>{
+   console.log("inside loginController");
+   const{email,password} = req.body
+   console.log(email,password);
+   try{
+      // check mail in model
+      const existingUser = await users.findOne({email})
+      if(existingUser){
+         if(password == existingUser.password){ 
+         //generate token
+         const token = jwt.sign({userMail:existingUser.email,role:existingUser.role},process.env.JWTSECRET)  
+            res.status(200).json({user:existingUser,token})
+         }else{
+          res.status(401).json("Incorrect Email / Password")         
+         }
+      }else{
+         res.status(404).json("Account Does Not Exists!!!")
+      }
+   }catch(error){
       console.log(error);
       res.status(500).json(error)
       
